86b4ee06c38f7922dd0f3f5d999ee1f993c7e76f
DDD <DDD@IT471NBMSK.ozon>
2020-03-02 17:41:55 +0300

    added second php

--- index.php
+++ index.php
@@ -1,5 +1,1 @@
 <?php include_once("main_page.html"); ?>
-<?php include_once("What_Zeus_likes.html"); ?>
-<?php include_once("Zeus_daily_regime.html"); ?>
-<?php include_once("Zeus_photos.html"); ?>
-<?php include_once("Right_answers.html"); ?>
---
+++ index1.php
@@ -0,0 +1,4 @@
+<?php include_once("What_Zeus_likes.html"); ?>
+<?php include_once("Zeus_daily_regime.html"); ?>
+<?php include_once("Zeus_photos.html"); ?>
+<?php include_once("Right_answers.html"); ?>
